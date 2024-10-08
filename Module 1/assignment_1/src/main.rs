845a7780bc27834f330e4d36baf0a6040f42665b
JulesCodehub <180057196+JulesCodehub@users.noreply.github.com>
2024-10-08 23:51:55 +0000

    moved freezing point to proper global variable and made proper immutable constant, defined conversion functions, created simple placeholder loop

--- Module 1/assignment_1/src/main.rs
+++ Module 1/assignment_1/src/main.rs
@@ -1,21 +1,26 @@
-// Implement the following functions
+// Define an Immutable constant for the freezing point of water, 32F
+const FREEZING_POINT: f64 = 32.0;
 
+// Conversion functions:
 fn fahrenheit_to_celsius(f: f64) -> f64 {
-    f + 1.0
+    (5.0 * (f - FREEZING_POINT)) / 9.0
 }
 
 fn celsius_to_fahrenheit(c: f64) -> f64 {
-    c + 1.0
+    ((9.0* c) / 5.0) + FREEZING_POINT
 }
 
 fn main() {
-    // Define an Immutable constant for the freezing point of water, 32F
-    let freezing_point = 32;
 
     // Define a Mutable variable for some temperature xxF
-    let mut temperature = 16;
+    let mut temperature = 16.0;
 
     // Convert to celcius and print result
+    println!("Original temperature: {}", temperature);
+    println!("Conversion to Celsius: {}", fahrenheit_to_celsius(temperature));
 
     // Use a loop to convert the next 5 integers
+    for offset in 1..6 {
+        println!("{}", offset);
+    }
 }
