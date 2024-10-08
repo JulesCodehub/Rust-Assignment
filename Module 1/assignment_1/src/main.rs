d2862e54a610c4507c912f4f6bbce2027ee61a57
JulesCodehub <180057196+JulesCodehub@users.noreply.github.com>
2024-10-08 23:30:18 +0000

    requirements outline for module 1 assignment 1

--- Module 1/assignment_1/src/main.rs
+++ Module 1/assignment_1/src/main.rs
@@ -1,3 +1,21 @@
+// Implement the following functions
+
+fn fahrenheit_to_celsius(f: f64) -> f64 {
+    f + 1.0
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    c + 1.0
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Define an Immutable constant for the freezing point of water, 32F
+    let freezing_point = 32;
+
+    // Define a Mutable variable for some temperature xxF
+    let mut temperature = 16;
+
+    // Convert to celcius and print result
+
+    // Use a loop to convert the next 5 integers
 }
