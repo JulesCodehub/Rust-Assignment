e26558b6ed1a37fa00e4fbb4d0f227c6fc94d2d4
JulesCodehub <180057196+JulesCodehub@users.noreply.github.com>
2024-10-09 04:10:32 +0000

    assignment 3 program outline and check_guess function completed

--- Module 1/assignment_3/src/main.rs
+++ Module 1/assignment_3/src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
     println!("Hello, world!");
 }
+fn check_guess(guess: i32, secret: i32) -> i32 {
+    
+    if guess == secret {
+        return 0
+    }
+    
+    if guess > secret {
+        return 1
+    }
+    
+    -1
+}
+
+fn main() {
+    
+    let mut secret = 32;
+    let mut count = 0;
+    
+    let mut guess = 0;
+    
+    // main loop
+
+        // set the guess variable
+        
+        // call our check_guess function
+        
+        // print how our guess was
+        
+        // if correct, break
+
+    // print our result
+    
+    println!("Game Finished, took: {} attempts.", count);
+
+}
