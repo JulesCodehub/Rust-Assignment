c9220221de4924e3f3d89ef8e4ffb329c85456d8
JulesCodehub <180057196+JulesCodehub@users.noreply.github.com>
2024-10-09 04:29:29 +0000

    assignment 3 main loop completed, simple game solver implemented, portentially make this smarter via newtons method

--- Module 1/assignment_3/src/main.rs
+++ Module 1/assignment_3/src/main.rs
@@ -1,6 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
 fn check_guess(guess: i32, secret: i32) -> i32 {
     
     if guess == secret {
@@ -18,21 +15,39 @@ fn main() {
     
     let mut secret = 32;
     let mut count = 0;
-    
     let mut guess = 0;
     
     // main loop
-
+    loop {
+    
+        //increment our attempt count
+        count += 1;
+        
         // set the guess variable
+        guess = (rand::random::<f64>() * 50.0) as i32;
         
         // call our check_guess function
+        let var = check_guess(guess, secret);
         
         // print how our guess was
-        
-        // if correct, break
+        match var {
+            
+            1 => println!("Incorrect Guess: {}, Too high", guess),
+            -1 => println!("Incorrect Guess: {}, Too low", guess),
+            0 => {
+                println!("Correct Guess: {}", guess);
+                // if correct, game is over
+                break
+            },
+            _ => {
+                println!("Error, this statement should not have been reached");
+                break
+            }
+        }
+
+    }
 
     // print our result
-    
     println!("Game Finished, took: {} attempts.", count);
 
 }
