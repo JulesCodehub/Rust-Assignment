0a1c16b82dcd5c7485952579cd2db8eb7c7a6bf3
JulesCodehub <180057196+JulesCodehub@users.noreply.github.com>
2024-10-09 04:03:58 +0000

    assignment 2 fizzbuzz loop implemented

--- Module 1/assignment_2/src/main.rs
+++ Module 1/assignment_2/src/main.rs
@@ -5,13 +5,24 @@ fn is_even(n: i32) -> bool {
 fn main() {
     
     //10 random numbers
-    let my_ten_numbers: [i32; 10] = [ 16, 21, 24, 23, 16, 13, 10, 18, 2, 10 ];
-    
-    //for idx in 0..my_ten_numbers.len() {
-    //    println!("{}", is_even(my_ten_numbers[idx]));
-    //}
+    let my_ten_numbers: [i32; 10] = [ 16, 21, 24, 23, 16, 13, 10, 15, 2, 10 ];
     
     // fizz buzz loop
+    for idx in 0..my_ten_numbers.len() {
+        let var = my_ten_numbers[idx];
+        if var % 3 == 0 && var % 5 == 0 {
+            println!("FizzBuzz");
+        } else if var % 3 == 0 {
+            println!("Fizz");
+        } else if var % 5 == 0 {
+            println!("Buzz");
+        } else if is_even(var) {
+            println!("Even");
+        } else {
+            println!("Odd");
+        }
+        
+    }
     
     // summation loop
     let mut sum = 0;
