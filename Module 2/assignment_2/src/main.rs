9396559ac369e86ee95c04d7ad3134909b86f1d7
JulesCodehub <180057196+JulesCodehub@users.noreply.github.com>
2024-10-09 05:41:44 +0000

    module 2 assingment 2 finished main loop and respected return types

--- Module 2/assignment_2/src/main.rs
+++ Module 2/assignment_2/src/main.rs
@@ -4,36 +4,43 @@ fn most_frequent_word(text: &str) -> (String, usize) {
     
     //Rust does not allow arrays to have sized not defined at compile time
     //The next simpliest data structure we can use is a vector, but it will be used exactly like an array
-    let mut count = vec![0; words.len()];
+    let mut count = vec![0 as i32; words.len()];
     
     for idx in 0..words.len(){
         
         // -1 will indicate that we have already looked at this word
-        if count[idx] != 1 {
+        if count[idx] != -1 {
             
             // we increment the count after having encountered this word
             count[idx] += 1;
             
             // we compare this word with the remaining words in the list looking for any duplicates
-            for idx2 in idx..words.len(){
+            for idx2 in idx+1..words.len(){
             
-            // when we encounter a duplicate, we increment our count and mark that spot with -1
-            
+                // when we encounter a duplicate
+                if words[idx] == words[idx2]{
+                
+                    //we increment our count
+                    count[idx] += 1;
+                
+                    // and mark that spot with -1
+                    count[idx2] = -1;
+                }
             }
-            //we keep going untill we have counted for all words
         }
     }
     
     // we then iterate through our counts, looking for the highest
-    let mut max_count = 0;
+    let mut counter = 0;
     let mut idx = 0;
     for idx2 in 0..words.len(){
-        if count[idx2] > max_count {
-            max_count = count[idx2];
+        if count[idx2] > counter {
+            counter = count[idx2];
             idx = idx2;
         }
     }
     
+    let max_count = counter as usize;
     let max_word = words[idx].to_string();
     
     (max_word, max_count) // return tuple
