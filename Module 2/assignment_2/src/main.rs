2a05f8fd0ba20344382ab375c4fcada2d3f830e2
JulesCodehub <180057196+JulesCodehub@users.noreply.github.com>
2024-10-09 04:55:40 +0000

    module 2 assignment 2 example code added

--- Module 2/assignment_2/src/main.rs
+++ Module 2/assignment_2/src/main.rs
@@ -1,3 +1,11 @@
+fn most_frequent_word(text: &str) -> (String, usize) {
+    let max_word = "a".to_string();
+    let max_count = 0;
+    (max_word, max_count) // return tuple
+}
+
 fn main() {
-    println!("Hello, world!");
+    let text = "the quick brown fox jumps over the lazy dog the quick brown fox";
+    let (word, count) = most_frequent_word(text);
+    println!("Most frequent word: \"{}\" ({} times)", word, count);
 }
