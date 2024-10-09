1c9ab6b02b6ec3175a76390569c6a38a24b20598
JulesCodehub <180057196+JulesCodehub@users.noreply.github.com>
2024-10-09 00:47:03 +0000

    assignment 2 outline

--- Module 1/assignment_2/src/main.rs
+++ Module 1/assignment_2/src/main.rs
@@ -1,3 +1,15 @@
+fn is_even(n: i32) -> bool {
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    //10 random numbers
+    let my_ten_numbers: [i32; 10] = [ 16, 21, 24, 23, 16, 13, 10, 18, 2, 10 ];
+    
+    // fizz buzz loop
+    
+    // summation loop
+    
+    // largest number loop
 }
